83f03e6d3c1dbcec57dca55d6697251f14ee239a
congtan1610 <congtan125@gmail.com>
2023-02-03 18:57:27 +0700

    up

--- ss08/exercise/validate_register/src/main/java/com/example/validate_register/repository/IUserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.validate_register.repository;
-
-import com.example.validate_register.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-public interface IUserRepository extends JpaRepository<User, Integer> {
-}
--- ss08/exercise/validate_register/src/main/java/com/example/validate_register/service/IUserService.java
+++
@@ -1,9 +0,0 @@
-package com.example.validate_register.service;
-
-import com.example.validate_register.model.User;
-
-import java.util.List;
-
-public interface IUserService {
-    void save(User user);
-}
