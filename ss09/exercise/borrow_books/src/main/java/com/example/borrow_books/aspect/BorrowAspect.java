bc761e2511299bc99c4929e0711fbe0f58553152
congtan1610 <congtan125@gmail.com>
2023-02-08 19:34:56 +0700

    up ss09

--- ss09/exercise/borrow_books/src/main/java/com/example/borrow_books/aspect/BorrowAspect.java
+++ ss09/exercise/borrow_books/src/main/java/com/example/borrow_books/aspect/BorrowAspect.java
@@ -3,14 +3,25 @@ package com.example.borrow_books.aspect;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class BorrowAspect {
+    private int visitedNumber =0;
     @After("execution(* com.example.borrow_books.controller.BorrowController.*(..))")
     public void logAfterMethod(JoinPoint joinPoint) {
         String name = joinPoint.getSignature().getName();
         System.out.println(name);
+    }
+    @Pointcut("execution(* com.example.borrow_books.service.IBorrowService.*(..))")
+    public void afterVisitedController(){
+    }
+
+    @After("afterVisitedController()")
+    public void throwAfterVisitedController(){
+        visitedNumber++;
+        System.out.println("Current visit count is "+ visitedNumber);
     }
 }
