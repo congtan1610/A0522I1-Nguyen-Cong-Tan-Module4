c045be041233ab7c5611aba2f208f371e123b3be
congtan1610 <congtan125@gmail.com>
2023-02-15 19:50:56 +0700

    up

---
+++ ss12/practice/phone_management/src/main/java/com/example/phone_management/repository/ISmartphoneRepository.java
@@ -0,0 +1,9 @@
+package com.example.phone_management.repository;
+
+import com.example.phone_management.model.Smartphone;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ISmartphoneRepository extends CrudRepository<Smartphone, Long> {
+}
---
+++ ss12/practice/phone_management/src/main/java/com/example/phone_management/service/ISmartphoneService.java
@@ -0,0 +1,16 @@
+package com.example.phone_management.service;
+
+
+import com.example.phone_management.model.Smartphone;
+
+import java.util.Optional;
+
+public interface ISmartphoneService {
+    Iterable<Smartphone> findAll();
+
+    Optional<Smartphone> findById(Long id);
+
+    Smartphone save(Smartphone smartPhone);
+
+    void remove(Long id);
+}
